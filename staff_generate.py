a02996e6928b025eed39642236c0571700ad5309
Kilian Soltermann <soltermann@puzzle.ch>
2023-02-22 11:07:23 +0100

    added .env file

--- staff_generate.py
+++ staff_generate.py
@@ -3,6 +3,7 @@
 # import pandas
 import pandas as pd
 import glob
+import os
 from random import randrange, shuffle
 import math
 
@@ -15,20 +16,7 @@ excel_file = pd.read_excel(io=file_name, sheet_name=sheet)
 
 def get_random_staff():
 
-    staff = [
-        "Cyrill Dubach",
-        "Cyrill Soltermann",
-        "Dominic Hänni",
-        "Fabian Kaufmann",
-        "Kvido Schmer",
-        "Mani Streit",
-        "Nicola Rauch",
-        "Nils Kipfer",
-        "Petz Balogh",
-        "Sebastian Kipfer",
-        "Tim Gerber",
-        "Toni Aschwanden",
-        ]
+    staff = os.getenv("stuff")
 
     shuffle(staff)
 
@@ -53,7 +41,7 @@ for row in excel_file.iloc:
         if len(staff) == 0:
             staff = get_random_staff()
         
-        date = str(row[0])
+        date = str(row[0])e
 
         if last_date != date:
                     
