c01cf9414933947cdf05d73e3b4ca333ce67c880
Kilian Soltermann <soltermann@puzzle.ch>
2025-01-14 08:28:15 +0100

    added some updates

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hbc_staff_tool
-This repository is used to generate the timekeeper staff list 
+This repository is used to generate the timekeeper staff list
+
+## how to run the script
+1. clone the repo from this [link](https://github.com/KiLLuuuhh/hbc-staff-tool):
+`git clone git@github.com:KiLLuuuhh/hbc-staff-tool.git`
+2. place the excel file in the root folder of the newly created repository.
+3. extend the staff list in the `.env` file with the current staff.
+4. install pipenv ([info](https://pipenv.pypa.io/en/latest/installation.html)) and run the following command:
+`pipenv install`
+5. run the script:
+`python staff_generate.py`
+
+
--- staff_generate.py
+++ staff_generate.py
@@ -4,8 +4,12 @@
 import pandas as pd
 import glob
 import os
+import random
 from random import randrange, shuffle
 import math
+from typing import List
+import ast
+
 
 # variables
 file_name = glob.glob("*.xlsx")[0]
@@ -14,15 +18,25 @@ last_date = ""
 
 excel_file = pd.read_excel(io=file_name, sheet_name=sheet)
 
-def get_random_staff():
+def get_random_staff() -> List[str]:
 
-    staff = os.getenv("stuff")
+    staff_string = os.getenv("staff")
 
-    shuffle(staff)
+    if not staff_string:
+        raise ValueError("Environment variable 'staff' is not set or is empty.")
 
-    return staff
+    # Parse the string into a list, handling quotes and commas
+    try:
+        staff_list = ast.literal_eval(f"[{staff_string}]")
+    except (ValueError, SyntaxError) as e:
+        raise ValueError("Failed to parse the 'staff' environment variable. Ensure it uses proper formatting.") from e
 
-def isnan(value):
+    # Shuffle the staff list
+    random.shuffle(staff_list)
+
+    return staff_list
+
+def isnan(value) -> bool:
     try:
         math.isnan(float(value))
 
@@ -35,26 +49,26 @@ staff = get_random_staff()
 
 for row in excel_file.iloc:
 
-    # check if M2 is reponsible
-    if isnan(row[0]) and "M2" in row[10]:
+    # check if M3 is reponsible
+    if isnan(row[0]) and "M3-Team 1" in row[10]:
 
         if len(staff) == 0:
             staff = get_random_staff()
-        
-        date = str(row[0])e
+
+        date = str(row[0])
 
         if last_date != date:
-                    
+
             staff_1 = staff.pop(randrange(len(staff)))
             staff_2 = staff.pop(randrange(len(staff)))
-        
-        
+
+
         time = str(row[1])
-        category = str(row[3])
-        home = str(row[4])
-        away = str(row[5])
+        category = str(row[2])
+        home = str(row[3]).strip()
+        away = str(row[4]).strip()
 
         last_date = date
 
-        # get two random stuff
-        print(f" {staff_1} {staff_2} {date} {time} {category} {home} {away} ")
+        # get two random staff
+        print(f"{staff_1} {staff_2} {date} {time} {category} {home} vs. {away} ")
